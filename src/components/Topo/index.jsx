1862a4077b5c4b182a092384af64d493bc0e579a
Dener de Jesus <ldenerjesus@gmail.com>
2024-07-30 19:21:32 -0300

    Iniciando projeto
    Primeiras configurações e estilizações
    componente topo sendo criado

---
+++ src/components/Topo/index.jsx
@@ -0,0 +1,10 @@
+import styles from './Topo.module.scss'
+
+export default function Topo(){
+    return (
+        <header className={styles.topo}>
+            <h1>RESTAURANT</h1>
+            <p>De pratos clássicos a criações surpreendentes, nosso cardápio é um requinte de sabores refinados.</p>
+        </header>
+    )
+}
